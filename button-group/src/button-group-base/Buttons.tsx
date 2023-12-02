bd4bfd80c02ffa38afb2a45b8322999b9ff2e48a
andyvernon <andy_vernon@live.co.uk>
2023-12-02 23:36:50 +0000

    Added click event

--- button-group/src/button-group-base/Buttons.tsx
+++ button-group/src/button-group-base/Buttons.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Container, ThemeProvider } from '@mui/material';
 import { ButtonGroup } from '@mui/material'
 import { ButtonsProps } from './Buttons.types';
@@ -8,17 +8,38 @@ import './Buttons.scss';
 
 const Buttons = (props: ButtonsProps) => {
 
+    const [selectedButton, setSelectedButton] = useState<number>(0);
+
     const bem = new BemIt('ButtonGroup');
+
+    const handleClick = (index: number) => {
+        setSelectedButton(index);
+    }
 
     return (
         <Container className={bem.out} fixed>
             <ThemeProvider theme={ButtonsTheme}>
                 <ButtonGroup>
-                    <Button className={bem.el('button').out}>Button 1</Button>
-                    <Button className={bem.el('button').out}>Button 2</Button>
-                    <Button className={bem.el('button').out}>Button 3</Button>
+                    <Button 
+                        className={bem.el('button').out} 
+                        onClick={() => handleClick(1)}>
+                            Button 1
+                    </Button>
+                    <Button 
+                        className={bem.el('button').out}
+                        onClick={() => handleClick(2)}>
+                            Button 2
+                    </Button>
+                    <Button 
+                        className={bem.el('button').out}
+                        onClick={() => handleClick(3)}>
+                            Button 3
+                    </Button>
                 </ButtonGroup>
             </ThemeProvider>
+            <div>
+                {selectedButton} is selected
+            </div>
         </Container>
     )
 }
